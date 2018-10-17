a0d26dd12925a76562010c0eea2acd6fcf2d1c06
Tzuri Kresh <tzurik@gmail.com>
2018-10-17 17:32:05 +0200

    test DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -97,4 +97,41 @@ describe('GET /todos/:id', () => {
           .expect(404)
           .end(done);
     });
-})
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var id = todos[1]._id.toHexString();
+
+        request(app)
+          .delete(`/todos/${id}`)
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.todo._id).toBe(id)
+          })
+          .end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+            Todo.findById(id).then((res) => {
+                expect(res).toNotExist();
+                done();
+            }).catch((err) => done(err));
+          })
+    });
+
+    it('sould return 404 if todo not found', (done) => {
+        var id = new ObjectID().toHexString();
+        request(app)
+          .delete(`/todos/${id}`)
+          .expect(404)
+          .end(done);
+    });
+    
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+          .delete('/todos/123')
+          .expect(404)
+          .end(done);
+    });
+});
