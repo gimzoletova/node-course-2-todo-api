b4fff981e38b548fcba38e6dcb5321555f7a400b
Tzuri Kresh <tzurik@gmail.com>
2018-10-17 14:56:43 +0200

    insert db URI for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //אומר למונגוס שנשתמש בספריית פרומיס המובנית בנודג'יאס ולא בספריית פרומיס אחרת
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoApp');
+mongoose.connect(process.env.PORT? 'mongodb://gimzoletova:@ds235053.mlab.com:35053/to_do_app' : 'mongodb://localhost:27017/todoApp');
 
 module.exports = {
     mongoose
