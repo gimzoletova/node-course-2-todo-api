576c61b0e8e0a091fb3d78f1b9e327e0498fd77b
Tzuri Kresh <tzurik@gmail.com>
2018-10-17 12:04:11 +0200

    add get req for todo by id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var { mongoose } = require('./db/mongoose')
-var { Todo } = require('./models/todo')
-var { User } = require('./models/user')
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -27,6 +28,20 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+    
+    Todo.findById(id).then((todo) => {
+        if(!todo)
+            return res.status(404).send();
+
+        res.send({todo});     
+    }).catch((err) => res.status(400).send());
 });
 
 app.listen(3000, () => console.log(`I'm listening on port 3000`));
