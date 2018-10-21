d30d7f383b6ee864c9fd44788f416c2f4ce9577b
Tzuri Kresh <tzurik@gmail.com>
2018-10-21 16:08:21 +0200

    add tests to POST/users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -207,7 +207,7 @@ describe('Post /users', () => {
                 expect(user).toExist();
                 expect(user.password).toNotBe(password);
                 done();
-              })
+              }).catch((err) => done(err));
           });
     });
 
@@ -226,4 +226,54 @@ describe('Post /users', () => {
           .expect(400)
           .end(done)
     });
+});
+
+describe('Post /users/login', () => {
+    it('should login user and return token', (done) => {
+        request(app)
+          .post('/users/login')
+          .send({
+            email: users[1].email,
+            password: users[1].password
+          })
+          .expect(200)
+          .expect((res) => {
+              expect(res.headers['x-auth']).toExist()
+          })
+          .end((err, res) => {
+            if(err)
+                return done(err);
+            
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens[0]).toInclude({
+                    access: 'auth',
+                    token: res.headers['x-auth']
+                });
+                done();
+            }).catch((err) => done(err));
+          });
+    });
+
+    it('should reject invalid login', (done) => {
+            request(app)
+              .post('/users/login')
+              .send({
+                email: users[1].email,
+                password: "justWrong"
+              })
+              .expect(400)
+              .expect((res) => {
+                  expect(res.headers['x-auth']).toNotExist()
+              })
+              .end((err, res) => {
+                if(err)
+                    return done(err);
+                
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((err) => done(err));
+              });
+    });
+    
 });
